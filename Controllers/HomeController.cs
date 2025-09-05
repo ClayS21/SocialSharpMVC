f27ab461ceee0457a6c29bd6daea233bb33f4414
Cleiton <cleiton.siqueira.266@gmail.com>
2025-09-04 23:23:54 -0300

    Create CSS file for index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialSharpMVC.Data;
 
 namespace SocialSharpMVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _context.Posts.Include(m => m.User).ToListAsync();
+            return View(allPosts);
         }
 
     }
