d909b00924d4bc30989200235249c2fec43b095f
Cleiton <cleiton.siqueira.266@gmail.com>
2025-09-07 15:19:17 -0300

    Add lower case URLs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 var dbConnectionString = builder.Configuration.GetConnectionString("Default");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(dbConnectionString));
